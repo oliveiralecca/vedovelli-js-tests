98b1337d96df6d37c26bdf3740f0bfc9284500df
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-07 22:06:51 -0300

    test: improves calculator tests

--- 01-introducao-simple-lib/src/lib/calculator.js
+++ 01-introducao-simple-lib/src/lib/calculator.js
@@ -1,3 +1,10 @@
-module.exports.sum = (num1, num2) => {
-  return num1 + num2;
+module.exports.sum = (a, b) => {
+  const number1 = parseInt(a);
+  const number2 = parseInt(b);
+
+  if (Number.isNaN(number1) || Number.isNaN(number2)) {
+    throw new Error('Please check your input');
+  }
+
+  return +number1 + +number2;
 }
--- 01-introducao-simple-lib/src/lib/calculator.spec.js
+++ 01-introducao-simple-lib/src/lib/calculator.spec.js
@@ -5,7 +5,7 @@
 *     - segundo parâmetro: método/função onde o teste será implementado e executado
 *   -> Dentro dos testes fazemos "assertions" (afirmações)
 *     - determinar se um método da aplicação retorna o que estou esperando que retorne
-*     - expect()
+*     - expect() => podem haver quantos forem preciso dentro de um mesmo it()/test()
 *   -> Verificando se o teste que passou não é um falso-positivo
 *     - uma das estratégias é usar a negação => expect().not.toBe()
 *     - caso o teste não passe, ele está correto e não é um falso-positivo
@@ -14,5 +14,31 @@
 const { sum } = require('./calculator');
 
 it('should sum 2 and 2 and the result must be 4', () => {
-  expect(sum(2, 2)).not.toBe(4);
+  expect(sum(2, 2)).toBe(4);
+});
+
+it('should sum 2 and 2 even if one of them is a string and the result must be 4', () => {
+  expect(sum('2', '2')).toBe(4);
+});
+
+it('should throw an error if what is provided to the method cannot be summed', () => {
+  // NaN
+  expect(() => {
+    sum('', '2')
+  }).toThrowError();
+
+  // array
+  expect(() => {
+    sum([2, 2])
+  }).toThrowError();
+
+  // object
+  expect(() => {
+    sum({})
+  }).toThrowError();
+
+  // empty args
+  expect(() => {
+    sum()
+  }).toThrowError();
 });
