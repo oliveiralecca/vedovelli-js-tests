084db6a21fd70307886dc8bdc35883008ee1f626
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-08 19:29:54 -0300

    refactor: refactors module to be cleaner

--- 01-introducao-simple-lib/src/libs/queryString.js
+++ 01-introducao-simple-lib/src/libs/queryString.js
@@ -1,7 +1,8 @@
-module.exports.queryString = (obj) => 
-  Object.entries(obj).map(([key, value]) => {
-    if (typeof value === 'object' && !Array.isArray(value)) {
-      throw new Error('Please check your args');
-    }
-    return `${key}=${value}`
-  }).join('&');
+const keyValueToString = ([key, value]) => {
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    throw new Error('Please check your args');
+  }
+  return `${key}=${value}`
+};
+
+module.exports.queryString = (obj) => Object.entries(obj).map(keyValueToString).join('&');
