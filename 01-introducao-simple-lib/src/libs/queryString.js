8bc0be9e5c6418b7724ca037d3d56ad3ea0440c7
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-09 15:29:13 -0300

    test: improves `queryString` tests suit

--- 01-introducao-simple-lib/src/libs/queryString.js
+++ 01-introducao-simple-lib/src/libs/queryString.js
@@ -2,7 +2,11 @@ const keyValueToString = ([key, value]) => {
   if (typeof value === 'object' && !Array.isArray(value)) {
     throw new Error('Please check your args');
   }
-  return `${key}=${value}`
+  return `${key}=${value}`;
 };
 
-module.exports.queryString = (obj) => Object.entries(obj).map(keyValueToString).join('&');
+module.exports.queryString = (obj) =>
+  Object.entries(obj).map(keyValueToString).join('&');
+
+module.exports.parse = (string) =>
+  Object.fromEntries(string.split('&').map((item) => item.split('=')));
--- 01-introducao-simple-lib/src/libs/queryString.spec.js
+++ 01-introducao-simple-lib/src/libs/queryString.spec.js
@@ -1,21 +1,21 @@
 /*
-* (2) Introdução aos testes
-*   -> Agrupar casos de teste => describe()
-*     - primeiro parâmetro: descrição do grupo de testes
-*     - segundo parâmetro: método/função onde virão os it() ou test()
-*   -> Diferença entre .toBe() e .toEqual()
-*     - .toBe() checa se o valor recebido é exatamente igual ao esperado
-*     - .toEqual() checa se é semelhante, por exemplo, um objeto com os mesmos valores porém em ordem diferente
-*/
+ * (2) Introdução aos testes
+ *   -> Agrupar casos de teste => describe()
+ *     - primeiro parâmetro: descrição do grupo de testes
+ *     - segundo parâmetro: método/função onde virão os it() ou test()
+ *   -> Diferença entre .toBe() e .toEqual()
+ *     - .toBe() checa se o valor recebido é exatamente igual ao esperado
+ *     - .toEqual() checa se é semelhante, por exemplo, um objeto com os mesmos valores porém em ordem diferente
+ */
 
-const { queryString } = require('./queryString');
+const { queryString, parse } = require('./queryString');
 
 describe('Object to query string', () => {
   it('should create a valid query string when an object is provided', () => {
     const obj = {
       name: 'Letícia',
       age: 29,
-      profession: 'Developer'
+      profession: 'Developer',
     };
 
     expect(queryString(obj)).toBe('name=Letícia&age=29&profession=Developer');
@@ -24,7 +24,7 @@ describe('Object to query string', () => {
   it('should create a valid query string even when an array is passed as value', () => {
     const obj = {
       name: 'Letícia',
-      skills: ['JS', 'TS', 'React']
+      skills: ['JS', 'TS', 'React'],
     };
 
     expect(queryString(obj)).toBe('name=Letícia&skills=JS,TS,React');
@@ -35,16 +35,31 @@ describe('Object to query string', () => {
       name: 'Letícia',
       skills: {
         first: 'JS',
-        second: 'React'
-      }
+        second: 'React',
+      },
     };
 
     expect(() => {
-      queryString(obj)
+      queryString(obj);
     }).toThrowError();
   });
 });
 
-// describe('Query string to object', () => {
-  
-// });
+describe('Query string to object', () => {
+  it('should convert a query string to object', () => {
+    const queryString = 'name=Letícia&profession=Developer';
+
+    expect(parse(queryString)).toEqual({
+      name: 'Letícia',
+      profession: 'Developer',
+    });
+  });
+
+  it('should convert a query string of a single key-value pair to object', () => {
+    const queryString = 'name=Letícia';
+
+    expect(parse(queryString)).toEqual({
+      name: 'Letícia',
+    });
+  });
+});
