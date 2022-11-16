4643f00d2eea64eccbe839b142053db82496b194
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-16 00:00:01 -0300

    refactor: adds `Dinero` lib and refactors the code to apply it

--- 01-introducao/02-shopping-cart/src/lib/Cart.spec.js
+++ 01-introducao/02-shopping-cart/src/lib/Cart.spec.js
@@ -28,7 +28,7 @@ describe('Cart', () => {
 
   describe('getTotal()', () => {
     it('should return 0 when getTotal() is executed in newly created instance', () => {
-      expect(cart.getTotal()).toEqual(0);
+      expect(cart.getTotal().getAmount()).toEqual(0);
     });
 
     it('should multiply quantity and price and receive the total amount', () => {
@@ -39,7 +39,7 @@ describe('Cart', () => {
 
       cart.add(item);
 
-      expect(cart.getTotal()).toEqual(70776);
+      expect(cart.getTotal().getAmount()).toEqual(70776);
     });
 
     it('should ensure no more than on product exists at a time', () => {
@@ -53,7 +53,7 @@ describe('Cart', () => {
         quantity: 1,
       });
 
-      expect(cart.getTotal()).toEqual(35388);
+      expect(cart.getTotal().getAmount()).toEqual(35388);
     });
 
     it('should update total when a product gets included and then removed', () => {
@@ -69,7 +69,7 @@ describe('Cart', () => {
 
       cart.remove(productA);
 
-      expect(cart.getTotal()).toEqual(41872);
+      expect(cart.getTotal().getAmount()).toEqual(41872);
     });
   });
 
@@ -100,7 +100,7 @@ describe('Cart', () => {
       });
 
       expect(cart.summary()).toMatchSnapshot();
-      expect(cart.getTotal()).toBeGreaterThan(0);
+      expect(cart.getTotal().getAmount()).toBeGreaterThan(0);
     });
 
     it('should reset the cart when checkout() is called', () => {
@@ -111,7 +111,7 @@ describe('Cart', () => {
 
       cart.checkout();
 
-      expect(cart.getTotal()).toEqual(0);
+      expect(cart.getTotal().getAmount()).toEqual(0);
     });
   });
 });
