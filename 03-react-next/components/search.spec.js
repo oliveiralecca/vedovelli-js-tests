a3eaef7954dcf5533d05bcb2c8c3289ef4fc5d47
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-21 16:30:58 -0300

    test(search): add a new test on `Search` component

--- 03-react-next/components/search.spec.js
+++ 03-react-next/components/search.spec.js
@@ -1,17 +1,33 @@
 /*
-*   -> Ferramentas adicionadas pela Testing Library
-*     - render => monta o componente
-*     - screen => faz referência a qualquer coisa retornada pelo componente
-*     - screen.debug() => ver o que está sendo retornado do componente diretamente no terminal
-*/
+ *   -> Ferramentas adicionadas pela Testing Library
+ *     - render => monta o componente
+ *     - screen => faz referência a qualquer coisa retornada pelo componente
+ *     - screen.debug() => ver o que está sendo retornado do componente diretamente no terminal
+ *     - fireEvent => dispara eventos, retornando uma Promise (tem que usar async/await)
+ *   -> Ferramentas do próprio Jest
+ *     - .toHaveBeenCalledTimes(1) => determina quantas vezes quero que um método seja executado
+ *     - jest.fn() => provê uma mock function para fins de testes
+ */
 
 import Search from './search';
-import { render, screen } from '@testing-library/react'; 
+import { render, screen, fireEvent } from '@testing-library/react';
+
+const doSearch = jest.fn();
 
 describe('Search', () => {
-  it('should render Search component', () => {
-    render(<Search />);
+  it('should render a form', () => {
+    render(<Search doSearch={doSearch} />);
 
-    expect(screen.getByTestId('search')).toBeInTheDocument();
+    expect(screen.getByRole('form')).toBeInTheDocument();
+  });
+
+  it('should call props.doSearch() when form is submitted', async () => {
+    render(<Search doSearch={doSearch} />);
+
+    const form = screen.getByRole('form');
+
+    await fireEvent.submit(form);
+
+    expect(doSearch).toHaveBeenCalledTimes(1);
   });
 });
