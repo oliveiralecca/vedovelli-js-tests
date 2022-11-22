9f6fe6c12ac41736b222994f183d46c0c5fba92d
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-22 16:39:12 -0300

    chore: add new config about jest coverage

--- 03-react-next/jest.config.js
+++ 03-react-next/jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
   },
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.js',
+    '<rootDir>/pages/**/*.js',
+  ],
 };
