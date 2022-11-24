37e2d73fd00edfb62f3f72ec6ba4899f2cebd3ba
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-24 19:41:22 -0300

    test(product-list): improve suite test

--- 03-react-next/pages-tests/product-list.integration.spec.js
+++ 03-react-next/pages-tests/product-list.integration.spec.js
@@ -1,10 +1,34 @@
-import { screen, render } from '@testing-library/react';
+/*
+*   -> Para os integration tests precisamos de uma fonte de dados, mas uma fonte de dados local, não uma fonte de dados externa. Porque os testes jamais podem depender de algo que não temos controle, como dados vindos de uma api
+*     - o MirageJS serve para isso => construir uma base de dados local, no front-end
+*/
+
+import { screen, render, waitFor } from '@testing-library/react';
 import ProductList from '../pages';
+
+const renderProductList = () => {
+  render(<ProductList />);
+};
 
 describe('ProductList', () => {
   it('should render ProductList', () => {
-    render(<ProductList />);
+    renderProductList();
 
     expect(screen.getByTestId('product-list')).toBeInTheDocument();
   });
+
+  fit('should render the ProductCard component 10 times', async () => {
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getAllByTestId('product-card')).toHaveLength(10);
+    });
+  });
+
+  it.todo('should render the no products message');
+  it.todo('should render the Search component');
+  it.todo('should filter the product list when a search is performed');
+  it.todo('should display error message when promise rejects');
+  it.todo('should display the total quantity of products');
+  it.todo('should display product (singular) when there is only 1 product');
 });
