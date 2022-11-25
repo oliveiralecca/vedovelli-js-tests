ab4ca05dd35bc8c264b13294dad519232d4f848f
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-25 12:52:19 -0300

    test(product-list): add one more test and create hooks folder

--- 03-react-next/pages-tests/product-list.integration.spec.js
+++ 03-react-next/pages-tests/product-list.integration.spec.js
@@ -1,23 +1,38 @@
 /*
-*   -> Para os integration tests precisamos de uma fonte de dados, mas uma fonte de dados local, não uma fonte de dados externa. Porque os testes jamais podem depender de algo que não temos controle, como dados vindos de uma api
-*     - o MirageJS serve para isso => construir uma base de dados local, no front-end
-*/
+ *   -> Para os integration tests precisamos de uma fonte de dados, mas uma fonte de dados local, não uma fonte de dados externa. Porque os testes jamais podem depender de algo que não temos controle, como dados vindos de uma API
+ *     - o MirageJS serve para isso => construir uma base de dados local, no front-end
+ *   -> O método waitFor() da Testing Library é utilizado quando tem uma operação assíncrona, da qual esperaremos o retorno de dados a serem renderizados, então tem que dar tempo ao react para que receba e renderize esses dados
+ *     - como uma chamada a uma API
+ */
 
 import { screen, render, waitFor } from '@testing-library/react';
 import ProductList from '../pages';
+import { makeServer } from '../miragejs/server';
 
 const renderProductList = () => {
   render(<ProductList />);
 };
 
 describe('ProductList', () => {
+  let server;
+
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
   it('should render ProductList', () => {
     renderProductList();
 
     expect(screen.getByTestId('product-list')).toBeInTheDocument();
   });
 
-  fit('should render the ProductCard component 10 times', async () => {
+  it('should render the ProductCard component 10 times', async () => {
+    server.createList('product', 10);
+
     renderProductList();
 
     await waitFor(() => {
