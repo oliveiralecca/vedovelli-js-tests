74879e0e6d12af8ef5bf0424ec0bc8750e72715c
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-29 14:46:31 -0300

    test(product-list): finish integration tests

--- 03-react-next/pages-tests/product-list.integration.spec.js
+++ 03-react-next/pages-tests/product-list.integration.spec.js
@@ -64,20 +64,20 @@ describe('ProductList', () => {
     });
   });
 
-  fit('should filter the product list when a search is performed', async () => {
+  it('should filter the product list when a search is performed', async () => {
     const searchTerm = 'Relógio bonito';
 
     server.createList('product', 2);
 
     server.create('product', {
       title: searchTerm,
-    })
+    });
 
     renderProductList();
 
     await waitFor(() => {
       expect(screen.getAllByTestId('product-card')).toHaveLength(3);
-    })
+    });
 
     const form = screen.getByRole('form');
     const input = screen.getByRole('searchbox');
@@ -87,9 +87,52 @@ describe('ProductList', () => {
 
     await waitFor(() => {
       expect(screen.getAllByTestId('product-card')).toHaveLength(1);
-    })
+    });
   });
 
-  it.todo('should display the total quantity of products');
-  it.todo('should display product (singular) when there is only 1 product');
+  it('should display the total quantity of products', async () => {
+    server.createList('product', 10);
+
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getByText(/10 Products/i)).toBeInTheDocument();
+    });
+  });
+
+  it('should display product (singular) when there is only 1 product', async () => {
+    server.create('product');
+
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getByText(/1 Product$/i)).toBeInTheDocument();
+    });
+  });
+
+  it('should display proper quantity when list is filtered', async () => {
+    const searchTerm = 'Relógio bonito';
+
+    server.createList('product', 2);
+
+    server.create('product', {
+      title: searchTerm,
+    });
+
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getByText(/3 Products/i)).toBeInTheDocument();
+    });
+
+    const form = screen.getByRole('form');
+    const input = screen.getByRole('searchbox');
+
+    await userEvent.type(input, searchTerm);
+    await fireEvent.submit(form);
+
+    await waitFor(() => {
+      expect(screen.getByText(/1 Product$/i)).toBeInTheDocument();
+    });
+  });
 });
