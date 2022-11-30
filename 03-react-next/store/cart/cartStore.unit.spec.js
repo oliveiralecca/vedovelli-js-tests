4caf0cfa9cf5ceffc5a4e57e837fa6ed7902156c
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-30 14:03:29 -0300

    test(cart-store): write initial tests to cart store

---
+++ 03-react-next/store/cart/cartStore.unit.spec.js
@@ -0,0 +1,29 @@
+/*
+ *   -> O método act() da Testing Library React Hooks serve para executarmos ações dentro de um hook
+ */
+
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useCartStore } from '.';
+
+describe('Cart Store', () => {
+  it('should return open equals false on initial state', () => {
+    const { result } = renderHook(() => useCartStore());
+
+    expect(result.current.state.open).toBe(false);
+  });
+
+  it('should toggle open state', async () => {
+    const { result } = renderHook(() => useCartStore());
+    const {
+      actions: { toggle },
+    } = result.current;
+
+    expect(result.current.state.open).toBe(false);
+
+    act(() => toggle());
+    expect(result.current.state.open).toBe(true);
+
+    act(() => toggle());
+    expect(result.current.state.open).toBe(false);
+  });
+});
