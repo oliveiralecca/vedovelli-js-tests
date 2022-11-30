cda6f46644b27dc82cb9d0e25a4db57c26854174
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-11-30 13:46:03 -0300

    feat: create store and add funcionality of open and close cart

---
+++ 03-react-next/store/cart/index.js
@@ -0,0 +1,15 @@
+/*
+ *   -> A lib Zustand é uma alternativa ao Redux e à ContextAPI (do próprio React)
+ *     - recursos que são necessários quando diversos componentes precisam manipular os mesmos estados e então precisamos de states globais para não ficar passando esses states de prop em prop entre os componentes
+ */
+
+import create from 'zustand';
+
+export const useCartStore = create((set) => ({
+  state: {
+    open: false,
+  },
+  actions: {
+    toggle: () => set((store) => ({ state: { open: !store.state.open } })),
+  },
+}));
